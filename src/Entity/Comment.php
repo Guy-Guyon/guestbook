1eae7223c7a1e230be2b348f2670452746cd1c75
Guy Guyon <guyon.guy@sfr.fr>
2021-12-19 18:53:48 +0100

    asynchronous spam checking

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -27,6 +27,7 @@ class Comment
 
     /**
      * @ORM\Column(type="text")
+     * 
      */
     #[Assert\NotBlank]
     private $text;
@@ -53,6 +54,11 @@ class Comment
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $photoFilename;
+
+    /**
+     * @ORM\Column(type="string", length=255, options={"default": "submitted"})
+     */
+    private $state = 'submitted';
 
     public function __toString(): string
     {
@@ -140,6 +146,18 @@ class Comment
     public function setPhotoFilename(?string $photoFilename): self
     {
         $this->photoFilename = $photoFilename;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
 
         return $this;
     }
